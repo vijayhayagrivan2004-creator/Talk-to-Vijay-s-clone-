5b4df953de73a6d67480cde5871a6a3ae073729d
vijayhayagrivan2004-creator <vijayhayagrivan2004@gmail.com>
2026-04-19 20:55:03 +0530

    Update file-pyu.py

--- file-pyu.py
+++ file-pyu.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")   # or simple text test
+
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 import datetime
 import random
 import re
